186e8261a91da25ad7d3ca03ed97fcdf2c141954
Greg Woods <gregwoodslancs@gmail.com>
2020-04-06 15:16:31 +0100

    working on the blue pill, with a hal

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,19 @@
 //! Note: Without additional hardware, PC13 should not be used to drive an LED, see page 5.1.2 of
 //! the reference manual for an explanation. This is not an issue on the blue pill.
 
-#![deny(unsafe_code)]
 #![no_std]
 #![no_main]
 
 use panic_halt as _;
 
-use nb::block;
+//use nb::block;
 
-use stm32f1xx_hal::{
-    prelude::*,
-    pac,
-    timer::Timer,
-};
+
+use cortex_m::peripheral::syst::SystClkSource;
 use cortex_m_rt::entry;
-use embedded_hal::digital::v2::OutputPin;
+use stm32f1::stm32f103 as pac;
+
+//use embedded_hal::digital::v2::OutputPin;
 
 #[entry]
 fn main() -> ! {
@@ -27,30 +25,29 @@ fn main() -> ! {
     let cp = cortex_m::Peripherals::take().unwrap();
     // Get access to the device specific peripherals from the peripheral access crate
     let dp = pac::Peripherals::take().unwrap();
+    let rcc = dp.RCC;
 
-    // Take ownership over the raw flash and rcc devices and convert them into the corresponding
-    // HAL structs
-    let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
+    // Set up the SysTick peripheral.
+    let mut syst = cp.SYST;
+    syst.set_clock_source(SystClkSource::Core);
+    syst.set_reload(2_100_000); //internal in clock ticks
+    syst.enable_counter();
 
-    // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
-    // `clocks`
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    // APB Adv.Periph.Bus 2 port enable register. Enable port A
+    rcc.apb2enr.write(|w| w.iopaen().set_bit());        
+    let gpioa = dp.GPIOA;
 
-    // Acquire the GPIOC peripheral
-    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
+    // Set pin A7 as general purpose output, with push/pull mode, 2MHz max
+    gpioa.crl.write(|w| w.cnf7().bits(0b00).mode7().bits(0b10));
+    
+    // Restart the SysTick counter.
+    syst.clear_current();
 
-    // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
-    // in order to configure the port. For pins 0-7, crl should be passed instead.
-    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-    // Configure the syst timer to trigger an update every second
-    let mut timer = Timer::syst(cp.SYST, &clocks).start_count_down(1.hz());
-
-    // Wait for the timer to trigger an update and change the state of the LED
     loop {
-        block!(timer.wait()).unwrap();
-        led.set_high().unwrap();
-        block!(timer.wait()).unwrap();
-        led.set_low().unwrap();
+        // Toggle the LED every SysTick tick.
+        while !syst.has_wrapped() {};
+        gpioa.odr.write(|w| w.odr7().set_bit());
+        while !syst.has_wrapped() {};
+        gpioa.odr.write(|w| w.odr7().clear_bit());
     }
 }
