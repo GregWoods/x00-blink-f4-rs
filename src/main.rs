0689cf7e0f56424008a5a4ae79ca2c17939d37ce
Greg Woods <gregwoodslancs@gmail.com>
2020-04-25 17:12:21 +0100

    updating stm32f4 to 0.11, and other updates

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@
 use panic_halt as _;
 use cortex_m::peripheral::syst::SystClkSource;
 use cortex_m_rt::entry;
-use stm32f1::stm32f103 as pac;
+use stm32f4::stm32f411 as pac;
 
 
 #[entry]
@@ -29,12 +29,25 @@ fn main() -> ! {
     syst.set_reload(2_100_000); //internal in clock ticks
     syst.enable_counter();
 
-    // Enable port C on APB2 
-    rcc.apb2enr.write(|w| w.iopcen().set_bit());        
+    // Enable clock on port C
+//  rcc.apb2enr.write(|w| w.iopcen().set_bit());        //stm32f1 
+    rcc.ahb1enr.write(|w| w.gpiocen().set_bit());       //stm32f4
+
     let gpioc = dp.GPIOC;
 
     // Set pin C13 as general purpose output, with push/pull mode, 2MHz max
-    gpioc.crh.write(|w| w.cnf13().bits(0b00).mode13().bits(0b10));
+    //gpioc.apb2.write(|w| w.cnf13().bits(0b00)
+    //                      .mode13().bits(0b10));     //stm32f1
+
+    //gpioc.moder.write(|w| w.moder13().bits(0x01));      //General purpose output mode
+    gpioc.moder.write(|w| w.moder13().output());        
+    //gpioc.otyper.write(|w| w.ot13().clear_bit());       //Output push-pull
+    gpioc.otyper.write(|w| w.ot13().push_pull());
+    //gpioc.ospeedr.write(|w| w.ospeedr13().bits(0x00));  //low speed
+    gpioc.ospeedr.write(|w| w.ospeedr13().low_speed());
+    //gpioc.pupdr.write(|w| w.pupdr13().bits(0x00));      //No pull-up, pull-down (not needed, since we use Output: push-pull)    
+    gpioc.pupdr.write(|w| w.pupdr13().floating());        //helper method used instead of .bits() which is marked unsafe
+    
     
     // Restart the SysTick counter.
     syst.clear_current();
